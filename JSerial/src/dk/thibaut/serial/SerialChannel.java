f6eb708a569067e42de9f33cc6cb3621f43afacf
Thibaut DIRLIK <thibaut.dirlik@gmail.com>
2015-09-26 02:10:03 +0200

    Empty gh-pages

--- JSerial/src/dk/thibaut/serial/SerialChannel.java
+++
@@ -1,8 +0,0 @@
-package dk.thibaut.serial;
-
-import java.io.IOException;
-import java.nio.channels.ByteChannel;
-
-public interface SerialChannel extends ByteChannel {
-    void flush(boolean in, boolean out) throws IOException;
-}
